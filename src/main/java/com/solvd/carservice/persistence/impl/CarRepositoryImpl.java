16cdb8fa52db823294ae34d0ff2c5d4c3228e796
solvd <solvd@MacBook-Pro-solvd.local>
2023-12-22 16:37:20 +0300

    implemente mvc

--- src/main/java/com/solvd/carservice/persistence/impl/CarRepositoryImpl.java
+++ src/main/java/com/solvd/carservice/persistence/impl/CarRepositoryImpl.java
@@ -1,11 +1,22 @@
 package com.solvd.carservice.persistence.impl;
 
 import com.solvd.carservice.domain.Car;
+import com.solvd.carservice.domain.Company;
 import com.solvd.carservice.persistence.CarRepository;
+import com.solvd.carservice.persistence.ConnectionPool;
+
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
 public class CarRepositoryImpl implements CarRepository {
+    private static final ConnectionPool CONNECTION_POOL = ConnectionPool.getInstance();
+    private static final String INSERT_CAR_QUERY = "INSERT INTO cars(brand, model, year) values (?, ?, ?);";
+    private static final String DELETE_CAR_QUERY = "DELETE FROM cars where id = ?;";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM cars where name = ?;";
+    private static final String FIND_BY_BRAND_QUERY = "SELECT * FROM cars where brand = ?;";
+    private static final String FIND_BY_MODEL_QUERY = "SELECT * FROM cars where model = ?;";
+    private static final String FIND_BY_YEAR_QUERY = "SELECT * FROM cars where year = ?;";
 
     @Override
     public List<Car> findByBrand(String brand) {
@@ -24,12 +35,38 @@ public class CarRepositoryImpl implements CarRepository {
 
     @Override
     public void create(Car car) {
-
+        Connection connection = CONNECTION_POOL.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CAR_QUERY, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, car.getBrand());
+            preparedStatement.setString(2, car.getModel());
+            preparedStatement.setInt(3, car.getYear());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while (resultSet.next()) {
+                car.setId(resultSet.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to create car", e);
+        } finally {
+            CONNECTION_POOL.releaseConnection(connection);
+        }
     }
 
     @Override
     public List<Car> findAll() {
-        return null;
+        List<Car> cars;
+        Connection connection = CONNECTION_POOL.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_QUERY);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            cars = mapCars(resultSet);
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to find all", e);
+        } finally {
+            CONNECTION_POOL.releaseConnection(connection);
+        }
+        return cars;
     }
 
     @Override
@@ -44,6 +81,15 @@ public class CarRepositoryImpl implements CarRepository {
 
     @Override
     public void deleteById(Long id) {
-
+        Connection connection = CONNECTION_POOL.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_CAR_QUERY);
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to delete company", e);
+        } finally {
+            CONNECTION_POOL.releaseConnection(connection);
+        }
     }
 }
--- src/main/java/com/solvd/carservice/persistence/impl/ClientRepositoryImpl.java
+++ src/main/java/com/solvd/carservice/persistence/impl/ClientRepositoryImpl.java
@@ -2,11 +2,20 @@ package com.solvd.carservice.persistence.impl;
 
 import com.solvd.carservice.domain.Client;
 import com.solvd.carservice.persistence.ClientRepository;
+import com.solvd.carservice.persistence.ConnectionPool;
+
 import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
 public class ClientRepositoryImpl implements ClientRepository {
+    private static final ConnectionPool CONNECTION_POOL = ConnectionPool.getInstance();
+    private static final String INSERT_CLIENT_QUERY = "INSERT INTO cars(brand, model, year) values (?, ?, ?);";
+    private static final String DELETE_CLIENT_QUERY = "DELETE FROM cars where id = ?;";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM cars where name = ?;";
+    private static final String FIND_BY_BRAND_QUERY = "SELECT * FROM cars where brand = ?;";
+    private static final String FIND_BY_MODEL_QUERY = "SELECT * FROM cars where model = ?;";
+    private static final String FIND_BY_YEAR_QUERY = "SELECT * FROM cars where year = ?;";
     @Override
     public List<Client> findByName(String name) {
         return null;
