c6481ec6e0e0486177ece2861edf2a2b92b27aa0
dafik-li <mhatmahandy@gmail.com>
2023-12-22 17:13:00 +0300

    implemente mvc

--- src/main/java/com/solvd/carservice/persistence/impl/ClientRepositoryImpl.java
+++ src/main/java/com/solvd/carservice/persistence/impl/ClientRepositoryImpl.java
@@ -4,18 +4,19 @@ import com.solvd.carservice.domain.Client;
 import com.solvd.carservice.persistence.ClientRepository;
 import com.solvd.carservice.persistence.ConnectionPool;
 
-import java.sql.Date;
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
 public class ClientRepositoryImpl implements ClientRepository {
     private static final ConnectionPool CONNECTION_POOL = ConnectionPool.getInstance();
-    private static final String INSERT_CLIENT_QUERY = "INSERT INTO cars(brand, model, year) values (?, ?, ?);";
-    private static final String DELETE_CLIENT_QUERY = "DELETE FROM cars where id = ?;";
-    private static final String FIND_ALL_QUERY = "SELECT * FROM cars where name = ?;";
-    private static final String FIND_BY_BRAND_QUERY = "SELECT * FROM cars where brand = ?;";
-    private static final String FIND_BY_MODEL_QUERY = "SELECT * FROM cars where model = ?;";
-    private static final String FIND_BY_YEAR_QUERY = "SELECT * FROM cars where year = ?;";
+    private static final String INSERT_CLIENT_QUERY = "INSERT INTO clients (brand, model, year) values (?, ?, ?);";
+    private static final String DELETE_CLIENT_QUERY = "DELETE FROM clients where id = ?;";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM clients where name = ?;";
+    private static final String FIND_BY_NAME_QUERY = "SELECT * FROM clients where name = ?;";
+    private static final String FIND_BY_SURNAME_QUERY = "SELECT * FROM clients where surname = ?;";
+    private static final String FIND_BY_PHONE_NUMBER_QUERY = "SELECT * FROM clients where phone_number = ?;";
+    private static final String FIND_BY_BIRTHDAY_QUERY = "SELECT * FROM clients where birthday = ?;";
     @Override
     public List<Client> findByName(String name) {
         return null;
@@ -37,8 +38,24 @@ public class ClientRepositoryImpl implements ClientRepository {
     }
 
     @Override
-    public void create(Client Entity) {
-
+    public void create(Client client) {
+        Connection connection = CONNECTION_POOL.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CLIENT_QUERY, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, client.getName());
+            preparedStatement.setString(2, client.getSurname());
+            preparedStatement.setString(3, client.getPhoneNumber());
+            preparedStatement.setDate(4, client.getBirthday());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while (resultSet.next()) {
+                client.setId(resultSet.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to create client", e);
+        } finally {
+            CONNECTION_POOL.releaseConnection(connection);
+        }
     }
 
     @Override
@@ -58,6 +75,15 @@ public class ClientRepositoryImpl implements ClientRepository {
 
     @Override
     public void deleteById(Long id) {
-
+        Connection connection = CONNECTION_POOL.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_CLIENT_QUERY);
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to delete client", e);
+        } finally {
+            CONNECTION_POOL.releaseConnection(connection);
+        }
     }
 }
--- src/main/java/com/solvd/carservice/persistence/impl/CostRepositoryImpl.java
+++ src/main/java/com/solvd/carservice/persistence/impl/CostRepositoryImpl.java
@@ -1,12 +1,18 @@
 package com.solvd.carservice.persistence.impl;
 
 import com.solvd.carservice.domain.Cost;
+import com.solvd.carservice.persistence.ConnectionPool;
 import com.solvd.carservice.persistence.CostRepository;
 
+import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
 public class CostRepositoryImpl implements CostRepository {
+    private static final ConnectionPool CONNECTION_POOL = ConnectionPool.getInstance();
+    private static final String INSERT_COST_QUERY = "INSERT INTO costs (brand, model, year) values (?, ?, ?);";
+    private static final String DELETE_COST_QUERY = "DELETE FROM costs where id = ?;";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM costs where cost = ?;";
 
     @Override
     public List<Cost> findByCost(Double cost) {
@@ -14,8 +20,23 @@ public class CostRepositoryImpl implements CostRepository {
     }
 
     @Override
-    public void create(Cost Entity) {
-
+    public void create(Cost cost) {
+        Connection connection = CONNECTION_POOL.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_COST_QUERY, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, cost.getCost());
+            preparedStatement.setString(2, cost.getSurname());
+            preparedStatement.setString(3, cost.getPhoneNumber());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while (resultSet.next()) {
+                cost.setId(resultSet.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to create client", e);
+        } finally {
+            CONNECTION_POOL.releaseConnection(connection);
+        }
     }
 
     @Override
